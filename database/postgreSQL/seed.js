018d1fe5bcdea755009a32d7cbe6014fec0fbaf7
Ingrid Carlson <ingridclaire@gmail.com>
2020-05-28 12:23:07 -0700

    removed csv file from repo, removed sample query from pg seed file

--- database/postgreSQL/seed.js
+++ database/postgreSQL/seed.js
@@ -1,9 +1,5 @@
 const db = require('./config.js');
 
-const simpleQuery = `INSERT INTO games (id, name, url, mainbody, sidebar, related) VALUES ($1, $2, $3, $4, $5, $6)`;
-
-const simpleValues = ['2', 'MarioKart', 'mariokart.com', JSON.stringify({ description: 'this is a really wonderful description' }), JSON.stringify({ vrsupport: 'it has lots of cool stuff' }), JSON.stringify(['English', 'French'])];
-
 const copyQueryStr = `COPY games FROM '/Volumes/MY PASSPORT/data.csv' WITH (DELIMITER '|', HEADER, FORMAT csv);`;
 
 db.query(copyQueryStr, (err, res) => {
